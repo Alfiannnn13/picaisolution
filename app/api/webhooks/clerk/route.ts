84af981999effcdea3b4bf767110be9a7630dbf5
Alfian <muhammadalfiannovanto17@gmail.com>
2024-07-27 20:25:38 +0700

    uroute1

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,11 +1,16 @@
 /* eslint-disable camelcase */
-
-import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
+import { createClerkClient } from "@clerk/backend"; // Import dari @clerk/backend
+import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
+
+// Buat instance clerkClient dengan secretKey Anda
+const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_API_KEY, // Pastikan ini diatur di variabel lingkungan Anda
+});
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
@@ -25,7 +30,7 @@ export async function POST(req: Request) {
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
+    return new Response("Error occurred -- no svix headers", {
       status: 400,
     });
   }
@@ -48,7 +53,7 @@ export async function POST(req: Request) {
     }) as WebhookEvent;
   } catch (err) {
     console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
+    return new Response("Error occurred", {
       status: 400,
     });
   }
@@ -65,8 +70,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name ?? '',
-      lastName: last_name ?? '',
+      firstName: first_name ?? '',  // Ganti null dengan string kosong
+      lastName: last_name ?? '',    // Ganti null dengan string kosong
       photo: image_url,
     };
 
@@ -89,8 +94,8 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name ,
+      firstName: first_name ?? '',  // Ganti null dengan string kosong
+      lastName: last_name ?? '',    // Ganti null dengan string kosong
       username: username!,
       photo: image_url,
     };
@@ -109,7 +114,7 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log(`Webhook with ID ${id} and type ${eventType}`);
   console.log("Webhook body:", body);
 
   return new Response("", { status: 200 });
