c918f7df8d7bb35a2990298c6582dea74aa57530
Alfian <muhammadalfiannovanto17@gmail.com>
2024-07-27 20:13:21 +0700

    uroute2

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -65,8 +65,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name ?? '',
+      lastName: last_name ?? '',
       photo: image_url,
     };
 
@@ -90,7 +90,7 @@ export async function POST(req: Request) {
 
     const user = {
       firstName: first_name,
-      lastName: last_name,
+      lastName: last_name ,
       username: username!,
       photo: image_url,
     };
