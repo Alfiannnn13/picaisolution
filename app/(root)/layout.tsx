24d27e1d25fd93e83412d9bd179fa1b822c471fd
Alfian <muhammadalfiannovanto17@gmail.com>
2024-07-29 16:47:25 +0700

    media uploader

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@
 
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
+import { Toaster } from '@/components/ui/toaster'
 
 const Layout = ({children}: {children:React.ReactNode}) => {
   return (
@@ -17,6 +18,7 @@ const Layout = ({children}: {children:React.ReactNode}) => {
                 {children}
             </div>
         </div>
+        <Toaster/>
     </main>
   )
 }
--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import Header from "@/components/shared/Header";
+import React from "react";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTransformationPage = () => {
+const AddTransformationPage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
   return (
-    <div>AddTransformationPage</div>
-  )
-}
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-export default AddTransformationPage
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  );
+};
+
+export default AddTransformationPage;
